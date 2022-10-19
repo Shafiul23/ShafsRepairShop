77672eea8f4365c9b2a9da5c2eeaf6cc56e7ffcd
Shafiul23 <shafiulmirza@hotmail.com>
2022-10-19 14:38:45 +0100

    login auth added to backend

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const notesController = require('../controllers/notesController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)
 
 router.route('/')
     .get(notesController.getAllNotes)
--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'));
+app.use('/auth', require('./routes/authRoutes'))
 app.use('/users', require('./routes/userRoutes'))
 app.use('/notes', require('./routes/notesRoutes'))
 
