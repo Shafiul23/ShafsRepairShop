ddf4f57e61365915161363a572799bcbe4dcaaf8
Shafiul23 <shafiulmirza@hotmail.com>
2022-10-20 09:36:53 +0100

    creating / updating usernames and notes are case insensitive

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -32,7 +32,7 @@ const createNewNote = asyncHandler(async (req, res) => {
     }
 
     //Check for duplicates
-    const duplicate = await Note.findOne({ title }).lean().exec()
+    const duplicate = await Note.findOne({ title }).collation({ locale: 'en', strength: 2 }).lean().exec()
     if (duplicate) {
         return res.status(409).json({ message: 'Duplicate note' })
     }
@@ -70,7 +70,7 @@ const updateNote = asyncHandler(async (req, res) => {
     }
 
     //Check for duplicate title
-    const duplicate = await Note.findOne({ title }).lean().exec()
+    const duplicate = await Note.findOne({ title }).collation({ locale: 'en', strength: 2 }).lean().exec()
 
     //Allow renaming of the original note
     if (duplicate && duplicate?._id.toString() !== id) {
--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -28,7 +28,7 @@ const createNewUser = asyncHandler(async (req, res) => {
     }
 
     //Check for duplicates
-    const duplicate = await User.findOne({ username }).lean().exec()
+    const duplicate = await User.findOne({ username }).collation({ locale: 'en', strength: 2 }).lean().exec()
     if (duplicate) {
         return res.status(409).json({ message: 'Duplicate username' })
     }
@@ -67,7 +67,7 @@ const updateUser = asyncHandler(async (req, res) => {
     }
 
     // Check for duplicate
-    const duplicate = await User.findOne({ username }).lean().exec()
+    const duplicate = await User.findOne({ username }).collation({ locale: 'en', strength: 2 }).lean().exec()
     // Allow updates to the original user
     if (duplicate && duplicate?._id.toString() !== id) {
         return res.status(409).json({ message: 'Duplicate username' })
