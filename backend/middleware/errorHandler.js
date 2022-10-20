0537fd342e7e34270dedf9c282bc0e897e07e645
Shafiul23 <shafiulmirza@hotmail.com>
2022-10-20 11:15:21 +0100

    fixed backend

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,4 +1,5 @@
 const { logEvents } = require('./logger')
+
 const errorHandler = (err, req, res, next) => {
     logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
     console.log(err.stack)
@@ -8,7 +9,7 @@ const errorHandler = (err, req, res, next) => {
 
     res.status(status)
 
-    res.json({ message: err.message })
+    res.json({ message: err.message, isError: true })
 
 }
 
--- backend/models/User.js
+++ backend/models/User.js
@@ -9,10 +9,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    roles: [{
-        type: String,
-        default: "Employee"
-    }],
+    roles: {
+        type: [String],
+        default: ["Employee"]
+    },
     active: {
         type: Boolean,
         default: true
